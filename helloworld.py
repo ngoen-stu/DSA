0ce434c202112fa5fc65768e3db255418e8f196f
ngoen-stu <154826209+ngoen-stu@users.noreply.github.com>
2023-12-25 23:30:07 +0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+# cau 1 
+print("Hello world")
+
+# cau 2
+hello = "Hello world"
+print(hello)
+
+# cau 3
+var1 = "Hello"
+var2 = "world"
+var3 = var1 + " " + var2
+print(var3)
+
+# cau 4
+def hamin(var):
+    print(var)
+hamin(var3)
